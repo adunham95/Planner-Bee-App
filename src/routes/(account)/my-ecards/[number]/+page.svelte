9d94c8471cf26d4927cef68ae8eeec48808efe0b
Adrian <adunham95@gmail.com>
2025-05-15 20:21:47 -0400

    Updated styles

--- src/routes/(account)/my-ecards/[number]/+page.svelte
+++ src/routes/(account)/my-ecards/[number]/+page.svelte
@@ -24,24 +24,33 @@
 		<StartingPointList
 			listItems={[
 				{
+					title: 'Copy Shareable Link',
+					description: 'Copy a link to share with family an friends',
+					onClick: () => alert('Clicked'),
+					// href: ``,
+					icon: share,
+					bgColor: 'bg-pastel-purple'
+				},
+				{
 					title: 'View ECard',
 					description: 'View the ecard',
 					href: `/ecard/${eCardID}`,
-					icon: ecard
+					icon: ecard,
+					bgColor: 'bg-pastel-lilac'
 				},
 				{
 					title: 'Cancel ECard',
 					description: 'Cancel the ecard, and make it not able to be seen or signed',
 					onClick: () => alert('Clicked'),
 					icon: close,
-					bgColor: 'bg-error-500'
+					bgColor: 'bg-pastel-orange'
 				},
 				{
 					title: 'Edit ECard',
 					description: 'Edit the ecard, make text or other changes',
 					onClick: () => alert('Clicked'),
 					icon: edit,
-					bgColor: 'bg-info-500'
+					bgColor: 'bg-pastel-sky'
 				},
 				{
 					title: 'Print ECard',
@@ -49,7 +58,7 @@
 					// onClick: () => alert('Clicked'),
 					href: `/my-ecards/${eCardID}/print`,
 					icon: print,
-					bgColor: 'bg-indigo-500'
+					bgColor: 'bg-pastel-teal'
 				}
 				// {
 				// 	title: 'View Analytics(Coming Soon)',
@@ -176,6 +185,23 @@
 			stroke-linecap="round"
 			stroke-linejoin="round"
 			d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"
+		/>
+	</svg>
+{/snippet}
+
+{#snippet share()}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="size-6"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
 		/>
 	</svg>
 {/snippet}
